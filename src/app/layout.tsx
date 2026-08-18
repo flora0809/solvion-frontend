9f17bb8f6a781605bbaf70085407e62c5f0229c2
flora0809 <89899754+flora0809@users.noreply.github.com>
2026-08-18 18:33:10 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.scss"
 import Header from "@/components/layout/Header"
 import BottomTab from "@/components/layout/BottomTab"
 import { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "SOLVION",
@@ -18,6 +19,15 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "y48lg0qll8");
+  `}
+</Script>
         <div className="app-container">
           <Header />
           <main className="main-content">{children}</main>
