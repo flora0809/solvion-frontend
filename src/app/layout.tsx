d1de4a59c7956273f635753d9839f29f376fd672
flora0809 <89899754+flora0809@users.noreply.github.com>
2026-08-22 14:01:54 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import "./globals.scss"
 import Header from "@/components/layout/Header"
 import BottomTab from "@/components/layout/BottomTab"
@@ -19,15 +18,20 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <Script id="microsoft-clarity" strategy="afterInteractive">
-  {`
-    (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "y48lg0qll8");
-  `}
-</Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-N9EYYG9XTF"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N9EYYG9XTF');
+          `}
+        </Script>
+
         <div className="app-container">
           <Header />
           <main className="main-content">{children}</main>
