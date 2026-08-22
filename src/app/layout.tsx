8fcdbdda0a6b49dcba7b9760c70fa51784cd7fa0
flora0809 <89899754+flora0809@users.noreply.github.com>
2026-08-22 17:23:48 +0900

    Update layout.tsx
    서치콘솔

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: "SOLVION",
   description: "SOLVION 모바일 앱",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  verification: {
+    google: "jhr8nGVdzeoB9JoOtGtHKGnkudRCUGUd4qg5k_Y3hfM",
+  },
 }
 
 export default function RootLayout({
@@ -22,7 +25,6 @@ export default function RootLayout({
           src="https://www.googletagmanager.com/gtag/js?id=G-N9EYYG9XTF"
           strategy="afterInteractive"
         />
-
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -31,7 +33,6 @@ export default function RootLayout({
             gtag('config', 'G-N9EYYG9XTF');
           `}
         </Script>
-
         <div className="app-container">
           <Header />
           <main className="main-content">{children}</main>
